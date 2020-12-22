ed45c833886f6ae32420ee14b58241ae797e2fdf
Jan de Visser <jan@de-visser.net>
2020-12-22 14:22:30 -0500

    Include reorg

--- heltec-gui/heltec-gui.ino
+++ heltec-gui/heltec-gui.ino
@@ -1,5 +1,10 @@
+
 #include "Arduino.h"
-#include "BLEDevice.h"
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
 #include <Preferences.h>
 
 #include "heltec.h"
